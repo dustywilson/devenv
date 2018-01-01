09267c73e99f7303a499a3447b6a0e26f7f060e3
Dusty Wilson <dusty@linux.com>
2018-01-01 05:34:32 -0800

    Big changes, small changelog

--- build.sh
+++ build.sh
@@ -3,14 +3,141 @@
 DISTRO_NAME=ubuntu
 DISTRO_VERSION=17.10
 PROTOC_VERSION=3.5.1
+HELM_VERSION=2.7.2
+GO_VERSION=1.9.2
+ATOM_VERSION=1.23.1
+GAESDK_VERSION=1.9.61
+NODE_REPOVER=9.x
+MONGO_REPOVER=3.6
+MONGO_DISTRO_NAME=xenial
+
 
 cd "$(dirname "$(readlink -f "$0")")"
 
+
+PREFIX_NAME="dustywilson/devenv"
+COMMIT_NAME="$(TZ=UTC git log --date=format-local:"%Y%m%d%H%m" --pretty=format:"%cd-%h" -n1)"
+NOT_COMMITTED=
+if git status --porcelain | egrep -i "^\s*[MD]" >/dev/null; then
+	COMMIT_NAME="$(date -u +"%Y%m%d%H%M-badwolf")"
+	NOT_COMMITTED=1
+fi
+VERSION="${COMMIT_NAME}-go${GO_VERSION}-node${NODE_REPOVER}-mongo${MONGO_REPOVER}-${DISTRO_NAME}${DISTRO_VERSION}-atom${ATOM_VERSION}"
+
+
+# Base Image (includes bare essentials, such as curl and git)
+
 docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	-t ${PREFIX_NAME}:base-${DISTRO_NAME}${DISTRO_VERSION} \
+	base
+
+
+# Programming Fonts Collection
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	-t ${PREFIX_NAME}:build-fonts \
+	fonts
+
+
+# Actions on Google
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	-t ${PREFIX_NAME}:build-gactions \
+	gactions
+
+
+# Protocol Buffers
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
 	--build-arg DISTRO_NAME=${DISTRO_NAME} \
 	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
 	--build-arg PROTOC_VERSION=${PROTOC_VERSION} \
-	-t dustywilson/devenv:build-protoc${PROTOC_VERSION}-${DISTRO_NAME}${DISTRO_VERSION} \
+	-t ${PREFIX_NAME}:build-protoc${PROTOC_VERSION}-${DISTRO_NAME}${DISTRO_VERSION} \
 	protoc
 
-docker push dustywilson/devenv:build-protoc${PROTOC_VERSION}-${DISTRO_NAME}${DISTRO_VERSION}
+
+# Kubernetes Helm
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	--build-arg HELM_VERSION=${HELM_VERSION} \
+	-t ${PREFIX_NAME}:build-helm${HELM_VERSION} \
+	helm
+
+
+# Go
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	--build-arg GO_VERSION=${GO_VERSION} \
+	-t ${PREFIX_NAME}:build-go${GO_VERSION} \
+	go
+
+
+# Google App Engine Go SDK
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	--build-arg GAESDK_VERSION=${GAESDK_VERSION} \
+	-t ${PREFIX_NAME}:build-gaesdk${GAESDK_VERSION} \
+	gaesdk
+
+
+# The Composite Build
+
+LATEST=
+[ -z $NOT_COMMITTED ] && LATEST="-t ${PREFIX_NAME}:latest"
+
+docker build \
+	--build-arg PREFIX_NAME=${PREFIX_NAME} \
+	--build-arg COMMIT_NAME=${COMMIT_NAME} \
+	--build-arg DISTRO_NAME=${DISTRO_NAME} \
+	--build-arg DISTRO_VERSION=${DISTRO_VERSION} \
+	--build-arg PROTOC_VERSION=${PROTOC_VERSION} \
+	--build-arg HELM_VERSION=${HELM_VERSION} \
+	--build-arg GO_VERSION=${GO_VERSION} \
+	--build-arg ATOM_VERSION=${ATOM_VERSION} \
+	--build-arg GAESDK_VERSION=${GAESDK_VERSION} \
+	--build-arg NODE_REPOVER=${NODE_REPOVER} \
+	--build-arg MONGO_REPOVER=${MONGO_REPOVER} \
+	--build-arg MONGO_DISTRO_NAME=${MONGO_DISTRO_NAME} \
+	-t ${PREFIX_NAME}:${VERSION}-${DISTRO_NAME}${DISTRO_VERSION} ${LATEST} \
+	devenv
+
+
+# Push!
+
+if [ -z $NOT_COMMITTED ]; then
+	docker push ${PREFIX_NAME}:base-${DISTRO_NAME}${DISTRO_VERSION}
+	docker push ${PREFIX_NAME}:build-fonts
+	docker push ${PREFIX_NAME}:build-gactions
+	docker push ${PREFIX_NAME}:build-protoc${PROTOC_VERSION}-${DISTRO_NAME}${DISTRO_VERSION}
+	docker push ${PREFIX_NAME}:build-helm${HELM_VERSION}
+	docker push ${PREFIX_NAME}:build-go${GO_VERSION}
+	docker push ${PREFIX_NAME}:build-gaesdk${GAESDK_VERSION}
+	docker push ${PREFIX_NAME}:${VERSION}-${DISTRO_NAME}${DISTRO_VERSION}
+	docker push ${PREFIX_NAME}:latest
+fi
---
+++ devenv/entrypoint.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+export USER="$(whoami)"
+
+if [ "$USER" != "root" ]; then
+	for PKG in \
+		go-plus \
+		go-debug \
+		go-signature-statusbar \
+		hyperclick \
+		linter \
+		linter-ui-default \
+		intentions \
+		busy-signal \
+		language-protobuf \
+		teletype
+		do [ ! -d "$HOME/.atom/packages/$PKG" ] && apm install $PKG
+	done
+fi
+
+[ "$TERM" == "dumb" ] && TERM=""
+export TERM=${TERM:-linux}
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export APPENGINEPATH=$HOME/go_appengine
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin:$APPENGINEPATH:$HOME/.npm-global/bin
+
+mkdir -p $GOPATH/src
+
+if [ "$USER" != "root" -a ! -d "$GOPATH/src/google.golang.org/grpc" ]; then
+	go get -u google.golang.org/grpc
+fi
+
+if [ "$USER" != "root" -a ! -d "$GOPATH/src/github.com/derekparker/delve/cmd/dlv" ]; then
+	go get -u github.com/derekparker/delve/cmd/dlv
+fi
+
+if [ "$USER" != "root" -a ! -d "$GOPATH/src/github.com/golang/protobuf/protoc-gen-go" ]; then
+	go get -u github.com/golang/protobuf/protoc-gen-go
+fi
+
+if [ "$1" == "-x" ]; then
+	# arg1 is -x, then the user wanted to run a command
+	# you'll want to `docker run` or `docker exec` with `-it` flag, or this won't likely work so well
+	shift
+	cd $HOME
+	$*
+elif [ "$1" == "-s" ]; then
+	shift
+	if [ -e "$1" ]; then
+		# arg1 is an existing directory, open shell with that directory
+		cd "$1"
+		/bin/bash
+	elif [ -e "$GOPATH/src/$1" ]; then
+		# arg1 is an existing Go package, open shell with that directory
+		cd "$GOPATH/src/$1"
+		/bin/bash
+	else
+		# no arg1 (or dir was not found), just open a shell at ~/go/src
+		cd /user/go/src
+		/bin/bash
+	fi
+elif [ "$1" == "-a" ]; then
+	shift
+	if [ -e "$1" ]; then
+		# arg1 is an existing directory, open Atom with that directory
+		atom -w "$1"
+	elif [ -e "$GOPATH/src/$1" ]; then
+		# arg1 is an existing Go package, open Atom with that directory
+		atom -w "$GOPATH/src/$1"
+	else
+		# run Atom with the go/src directory open (because we didn't find the requested dir, or there was none requested)
+		atom -w $GOPATH/src
+	fi
+else
+	# something else, give help
+	echo "Usage:"
+	echo "  (no arguments)		this help screen, then exit"
+	echo "  -x <somecommand>	to run a command"
+	echo "  -s			to run a shell at go/src dir"
+	echo "  -s <dir-path>		to run a shell at that dir"
+	echo "  -s <go-package-name>	to run a shell at that Go package dir"
+	echo "  -a			to run Atom with go/src dir"
+	echo "  -a <file-or-dir-path>	to run Atom with that file or dir"
+	echo "  -a <go-package-name>	to run Atom with that Go package"
+fi
