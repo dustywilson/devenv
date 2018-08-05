0515167241ffed399d5dd0a084edeee09f425555
Emmaly Wilson <emmaly@emma.ly>
2018-08-05 10:14:29 +0000

    example.sh improvements
    • Adds start of support for username flag
    • Added -S to run a root shell in the container
    • Added -X to run a command in the background, similar to how -a and -v work
    • Added volume entry for /user/.local/share (because Konsole stores profiles there)
    • Added --ipc=host because Atom and VS Code seem to run better with it

--- devenv/example.sh
+++ devenv/example.sh
@@ -11,6 +11,18 @@ D="${DEVENV_DIR:-${ODIR}}"
 P="${DEVENV_PORT:-8880}"
 R="$(docker ps -q --filter "name=^/${N}$")"
 E="$(docker ps -aq --filter "name=^/${N}$")"
+U=""
+
+#if [[ $1 = -u* ]]; then
+#	U=${1/#-u/}
+#	shift;
+#	if [ -z "$U" ]; then
+#		if [[ ! $1 = -* ]]; then
+#			U=$1
+#			shift;
+#		fi
+#	fi
+#fi
 
 echo
 echo "image    = [$I]"
@@ -19,7 +31,10 @@ echo "basedir  = [$D]"
 echo "baseport = [$P]"
 echo "running  = [$R]"
 echo "existing = [$E]"
+#[ ! -z "$U" ] && echo "user     = [$U]"
 echo
+
+#[ ! -z "$U" ] && U="-u $U"
 
 if [ "$1" == "-destroy" ]; then
 	if [ -z "$E" ]; then
@@ -34,18 +49,21 @@ elif [ "$1" == "-r" ]; then
 		"$D/go" \
 		"$D/atom" \
 		"$D/vscode" \
-		"$D/config"
+		"$D/config" \
+		"$D/local_share" \
 
 	[ ! -z "$E" ] && docker rm -f $N
 	docker run -d \
 		--name=$N \
 		--restart=always \
+		--ipc=host \
 		-e DISPLAY=$DISPLAY \
 		-v /tmp/.X11-unix/:/tmp/.X11-unix/ \
 		-v "$D/go":/user/go \
 		-v "$D/atom":/user/.atom \
 		-v "$D/vscode":/user/.vscode \
 		-v "$D/config":/user/.config \
+		-v "$D/local_share":/user/.local/share \
 		-p 0.0.0.0:$(( ${P} + 0 )):$(( ${P} + 0 )) \
 		-p 0.0.0.0:$(( ${P} + 1 )):$(( ${P} + 1 )) \
 		-p 0.0.0.0:$(( ${P} + 2 )):$(( ${P} + 2 )) \
@@ -64,14 +82,20 @@ elif [ -z "$E" ]; then
 elif [ -z "$R" ]; then
 	echo "Container [$N] exists, but is not running.  Maybe run: docker start ${N}"
 	exit 1
+elif [ "$1" == "-S" ]; then
+	# root shell
+	docker exec -u root -it $N /bin/bash
 else
 	if [ "$1" == "-s" -o "$1" == "-x" ]; then
-		docker exec -it $N ./entrypoint.sh $*
+		docker exec $U -it $N /user/entrypoint.sh $*
+	elif [ "$1" == "-X" ]; then
+		shift;
+		docker exec $U $N /user/entrypoint.sh -x $* &
 	elif [ "$1" == "-a" -o "$1" == "-v" ]; then
-		docker exec $N ./entrypoint.sh $* &
+		docker exec $U $N /user/entrypoint.sh $* &
 	elif [ "$1" == "" -o "$1" == "-h" -o "$1" == "--help" ]; then
-		docker exec -it $N ./entrypoint.sh
+		docker exec $U -it $N /user/entrypoint.sh
 	else
-		docker exec -it $N ./entrypoint.sh -x $*
+		docker exec $U -it $N /user/entrypoint.sh -x $*
 	fi
 fi
